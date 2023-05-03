797d31b9c4ebd90d71bc2c8437a167a0c7869d58
NelsonDong41 <nelsondong41@gmail.com>
2023-05-02 23:18:26 -0400

    started project from section 3

---
+++ Projects/section4/pages/events/[eventId].js
@@ -0,0 +1,16 @@
+import {useRouter} from "next/router";
+
+
+function EventDetailPage() {
+    const router = useRouter();
+
+    console.log(router.query);
+
+    return (<div>
+        <h1>
+            The Event Detail Page
+        </h1>
+    </div>);
+};
+
+export default EventDetailPage;
