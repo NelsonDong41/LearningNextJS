6c25b3f6033a555e64a419812893b60a78d037eb
NelsonDong41 <nelsondong41@gmail.com>
2023-05-03 15:59:37 -0400

    finish project

--- Projects/section4/pages/events/[...slug].js
+++ Projects/section4/pages/events/[...slug].js
@@ -1,16 +1,62 @@
-import {useRouter} from "next/router";
-
+import EventList from "@/components/events/event-list";
+import ResultsTitle from "@/components/events/results-title";
+import Button from "@/components/ui/button";
+import ErrorAlert from "@/components/ui/error-alert";
+import { getFilteredEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
 
 function FilteredEventsPage() {
-    const router = useRouter();
+  const router = useRouter();
 
-    console.log(router.query);
+  const filterData = router.query.slug;
 
-    return (<div>
-        <h1>
-            The Filtered Events Page
-        </h1>
-    </div>);
-};
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return (
+      <ErrorAlert>
+        <p>Invalid Filter please adjust your values</p>
+      </ErrorAlert>
+    );
+  }
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <>
+        <ErrorAlert>
+          <p>No Events found for the chosen filter</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </>
+    );
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+  return (
+    <>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </>
+  );
+}
 
 export default FilteredEventsPage;
--- Projects/section4/pages/events/[eventId].js
+++ Projects/section4/pages/events/[eventId].js
@@ -1,16 +1,39 @@
-import {useRouter} from "next/router";
+import { getEventById } from "@/dummy-data";
+import { useRouter } from "next/router";
 
+import EventSummary from "@/components/event-detail/event-summary";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventContent from "@/components/event-detail/event-content";
+import ErrorAlert from "@/components/ui/error-alert";
 
 function EventDetailPage() {
-    const router = useRouter();
+  const router = useRouter();
 
-    console.log(router.query);
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
 
-    return (<div>
-        <h1>
-            The Event Detail Page
-        </h1>
-    </div>);
-};
+  if (!event) {
+    return (
+      <ErrorAlert>
+        <p>No event found</p>
+      </ErrorAlert>
+    );
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
+}
 
 export default EventDetailPage;
