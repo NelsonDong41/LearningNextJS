c9a7e6c342b27d4acbf0fca75a62199c6203291c
NelsonDong41 <nelsondong41@gmail.com>
2023-05-19 13:38:16 -0400

    pushing

--- Projects/nextjs-course-code-26a6b230c965996f99bd616a2e8617d295a8efad/pages/api/comments/[eventId].js
+++ Projects/nextjs-course-code-26a6b230c965996f99bd616a2e8617d295a8efad/pages/api/comments/[eventId].js
@@ -1,40 +1,50 @@
-function handler(req, res) {
-    const eventId = req.query.eventId;
-  
-    if (req.method === 'POST') {
-      const { email, name, text } = req.body;
-  
-      if (
-        !email.includes('@') ||
-        !name ||
-        name.trim() === '' ||
-        !text ||
-        text.trim() === ''
-      ) {
-        res.status(422).json({ message: 'Invalid input.' });
-        return;
-      }
-  
-      const newComment = {
-        id: new Date().toISOString(),
-        email,
-        name,
-        text,
-      };
-  
-      console.log(newComment);
-  
-      res.status(201).json({ message: 'Added comment.', comment: newComment });
+import { MongoClient } from "mongodb";
+async function handler(req, res) {
+  const eventId = req.query.eventId;
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://nelsondong158:learningNextJs@cluster0.g7mbiwp.mongodb.net/events?retryWrites=true&w=majority"
+  );
+  if (req.method === "POST") {
+    const { email, name, text } = req.body;
+
+    if (
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !text ||
+      text.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid input." });
+      return;
     }
-  
-    if (req.method === 'GET') {
-      const dummyList = [
-        { id: 'c1', name: 'Max', text: 'A first comment!' },
-        { id: 'c2', name: 'Manuel', text: 'A second comment!' },
-      ];
-  
-      res.status(200).json({ comments: dummyList });
-    }
+
+    const newComment = {
+      email,
+      name,
+      text,
+      eventId,
+    };
+
+    const db = client.db();
+    const result = await db.collection("comments").insertOne(newComment);
+    console.log(result);
+
+    newComment.id = result.insertedId;
+    res.status(201).json({ message: "Added comment.", comment: newComment });
   }
-  
-  export default handler;
+
+  if (req.method === "GET") {
+    const db = client.db();
+    const documents = await db
+      .collection("comments")
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
+
+    res.status(200).json({ comments: documents });
+  }
+  client.close();
+}
+
+export default handler;
--- Projects/nextjs-course-code-26a6b230c965996f99bd616a2e8617d295a8efad/pages/api/newsletter.js
+++ Projects/nextjs-course-code-26a6b230c965996f99bd616a2e8617d295a8efad/pages/api/newsletter.js
@@ -1,17 +1,47 @@
-function handler(req, res) {
-    if(req.method === 'POST') {
-        const userEmail = req.body.email;
+import { MongoClient } from "mongodb";
 
-        if(!userEmail || !userEmail.includes('@')) {
-            res.status(422).json({message: 'Invalid email address/'});
-            return;
-        }
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://nelsondng158:learningNextJs@cluster0.g7mbiwp.mongodb.net/events?retryWrites=true&w=majority"
+  );
 
-        console.log(userEmail);
-        res.status(201).json({message: 'Signed up!'});
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+
+  await db.collection(document).insertOne({ email: userEmail });
+}
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const userEmail = req.body.email;
+
+    if (!userEmail || !userEmail.includes("@")) {
+      res.status(422).json({ message: "Invalid email address/" });
+      return;
+    }
+
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+        res.status(500).json({message: 'Connecting to the database failed!'});
+        return;
+    }
+
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+        res.status(500).json({message: 'Inserting data failed!'});
+        return;
     }
 
 
+    res.status(201).json({ message: "Signed up!" });
+  }
 }
 
 export default handler;
